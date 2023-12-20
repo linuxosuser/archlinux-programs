2acda9da774f63ebfc54ea2e6090f40eb9a03687
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2023-12-20 11:36:15 +0100

    Update Inspiron-3567-video-drivers.sh

--- Inspiron-3567-video-drivers.sh
+++ Inspiron-3567-video-drivers.sh
@@ -8,4 +8,7 @@ sudo pacman -S --needed intel-compute-runtime --noconfirm
 sudo pacman -S --needed xf86-video-amdgpu --noconfirm
 sudo pacman -S --needed vulkan-radeon --noconfirm
 sudo pacman -S --needed lib32-vulkan-radeon --noconfirm
-sudo pacman -S --needed rocm-opencl-runtime --noconfirm
+sudo pacman -S --needed libva-mesa-driver --noconfirm
+sudo pacman -S --needed lib32-libva-mesa-driver --noconfirm
+sudo pacman -S --needed mesa-vdpau --noconfirm
+sudo pacman -S --needed lib32-mesa-vdpau --noconfirm
