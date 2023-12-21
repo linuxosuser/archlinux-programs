9efbe7c8d51f335cbe9d7128f6c22a4d24296e77
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2023-12-21 21:14:20 +0100

    Update xfce-desktop-install.sh

--- xfce-desktop-install.sh
+++ xfce-desktop-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S --needed xorg xfce4 xfce4-goodies sddm gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-xapp xdg-desktop-portal-wlr xdg-user-dirs firefox firefox-i18n-hu firefox-ublock-origin chromium && sudo systemctl enable sddm.service && sudo systemctl set-default graphical.target && sudo systemctl enable sshd.service
+sudo pacman -S --needed xorg xfce4 xfce4-goodies sddm gvfs gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-xapp xdg-desktop-portal-wlr xdg-user-dirs network-manager-applet firefox firefox-i18n-hu firefox-ublock-origin chromium && sudo systemctl enable sddm.service && sudo systemctl set-default graphical.target && sudo systemctl enable sshd.service
