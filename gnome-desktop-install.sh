43ebd30670f4dc5706e962684d2269c1adcb6ec7
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2023-12-22 14:39:53 +0100

    Delete gnome-desktop-install.sh

--- gnome-desktop-install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S --needed xorg gnome gnome-tweaks gnome-terminal gnome-browser-connector firefox firefox-i18n-hu firefox-ublock-origin chromium && sudo systemctl enable gdm && sudo systemctl set-default graphical.target && sudo systemctl enable sshd
