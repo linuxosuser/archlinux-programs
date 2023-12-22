998bba1de54765c5ede72f0899111e5ec6f17a83
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2023-12-22 14:40:46 +0100

    Delete NUC8i5BEKPA-video-driver.sh

--- NUC8i5BEKPA-video-driver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S --needed xf86-video-intel --noconfirm
-sudo pacman -S --needed intel-media-driver --noconfirm
-sudo pacman -S --needed vulkan-intel --noconfirm
-sudo pacman -S --needed lib32-vulkan-intel --noconfirm
