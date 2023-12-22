a698690b7e6575a5d0927ddbc93ae0cb6fa443b8
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2023-12-22 14:40:19 +0100

    Delete Pipewire-sound-install.sh

--- Pipewire-sound-install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S --needed pipewire libpipewire pipewire-alsa pipewire-audio pipewire-pulse pipewire-jack pipewire-roc pipewire-v4l2 pipewire-zeroconf gst-plugin-pipewire lib32-pipewire lib32-libpipewire lib32-pipewire-v4l2 lib32-gst-plugins-good
