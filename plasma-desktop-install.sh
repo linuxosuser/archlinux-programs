234e486f9212f1868a4f066f21cb204b7e9884bb
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2023-12-31 00:30:02 +0100

    Update plasma-desktop-install.sh

--- plasma-desktop-install.sh
+++ plasma-desktop-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo pacman -S --needed xorg --noconfirm
+sudo pacman -S --needed pipewire-jack --noconfirm
 sudo pacman -S --needed plasma-meta --noconfirm
 sudo pacman -S --needed dolphin --noconfirm
 sudo pacman -S --needed dolphin-plugins --noconfirm
@@ -9,7 +10,6 @@ sudo pacman -S --needed sddm --noconfirm
 sudo pacman -S --needed xdg-desktop-portal --noconfirm
 sudo pacman -S --needed xdg-desktop-portal-kde --noconfirm
 sudo pacman -S --needed xdg-user-dirs --noconfirm
-sudo pacman -S --needed pipewire-jack --noconfirm
 sudo systemctl enable sddm.service
 sudo systemctl set-default graphical.target
 
