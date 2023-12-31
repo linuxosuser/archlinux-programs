edd37aafc9848684394917115a4ecffd1598888d
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2023-12-31 13:34:30 +0100

    Update xfce-desktop-install.sh

--- xfce-desktop-install.sh
+++ xfce-desktop-install.sh
@@ -21,4 +21,10 @@ sudo pacman -S --needed network-manager-applet --noconfirm
 sudo systemctl enable lightdm.service
 sudo systemctl set-default graphical.target
 
+echo -n 'Section "InputClass"
+        Identifier "system-keyboard"
+        MatchIsKeyboard "on"
+        Option "XkbLayout" "hu"
+EndSection' > /etc/X11/xorg.conf.d/00-keyboard.conf
+
 echo 'A TELEPÍTÉS BEFEJEZŐDÖTT, KÉREM, HOGY INDÍTSA ÚJRA A SZÁMÍTÓGÉPÉT!'
