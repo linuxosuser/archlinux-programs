dda2673234f8d503244060153ecf9f42aa54f8f9
linuxosuser <154092858+linuxosuser@users.noreply.github.com>
2023-12-31 13:35:36 +0100

    Update plasma-desktop-install.sh

--- plasma-desktop-install.sh
+++ plasma-desktop-install.sh
@@ -13,4 +13,10 @@ sudo pacman -S --needed xdg-user-dirs --noconfirm
 sudo systemctl enable sddm.service
 sudo systemctl set-default graphical.target
 
+echo -n 'Section "InputClass"
+        Identifier "system-keyboard"
+        MatchIsKeyboard "on"
+        Option "XkbLayout" "hu"
+EndSection' > /etc/X11/xorg.conf.d/00-keyboard.conf
+
 echo 'A TELEPÍTÉS BEFEJEZŐDÖTT, KÉREM, HOGY INDÍTSA ÚJRA A SZÁMÍTÓGÉPÉT!'
